c20aa7caa3bdea8251347fa25986ac8447451125
sagara <sagara@smashataps.com>
2023-07-08 23:32:38 +0530

    trigger deployement 7

--- infrastructure/cdk/lib/backend-stack.ts
+++ infrastructure/cdk/lib/backend-stack.ts
@@ -1,4 +1,5 @@
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
@@ -10,7 +11,12 @@ export class BackendStack extends cdk.Stack {
     const portfolioBucket = new s3.Bucket(this, `MyBucket-${appName}`, {
       versioned: true,
     });
-    
+
+    portfolioBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [portfolioBucket.arnForObjects('*')],
+      principals: [new iam.AnyPrincipal()],
+    }));
     new cdk.CfnOutput(this, "BucketName", {
       value: portfolioBucket.bucketName,
     });
